ebb0514a942982743b54c9a2c736ed75a88e6b4f
judperezre <168015658+judperezre@users.noreply.github.com>
2025-08-11 17:30:15 -0500

    correcion del proyecto

--- portfolio-backend/server.js
+++ portfolio-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
+const path = require('path');
 dotenv.config();
 const app = express();
 app.use(express.json());
